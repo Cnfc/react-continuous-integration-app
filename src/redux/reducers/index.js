f85363a3aced400723f5a737546b91faa4497e20
Stanislav Dashkov <stdashkov@mail.ru>
2020-10-12 18:07:25 +0300

    add basic setup with redux

---
+++ src/redux/reducers/index.js
@@ -0,0 +1,12 @@
+import { combineReducers } from "redux";
+
+import errorReducer from "./errorReducer";
+import movieReducer from "./movieReducer";
+
+const rootReducer = combineReducers({
+  errors: errorReducer,
+  movies: movieReducer,
+  openMenu: ""
+});
+
+export default rootReducer;
